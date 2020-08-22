154f5a1450688ceb44f0ff3588a1678e94a36dd8
JayH <holliday.jay@gmail.com>
2020-08-22 11:06:23 -0400

    PWA

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
+let cacheName = "Sherwin";
 // Install event
 self.addEventListener("install", function(e) {
-    console.log("Alloy service worker installation");
+    console.log("Sherwin service worker installation");
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            console.log("Alloy service worker caching dependencies");
+            console.log("Sherwin service worker caching dependencies");
             initialCache.map(function(url) {
                 return cache.add(url).catch(function(reason) {
                     return console.log(
-                        "Alloy: " + String(reason) + " " + url
+                        "Sherwin: " + String(reason) + " " + url
                     );
                 });
             });
@@ -17,13 +18,13 @@ self.addEventListener("install", function(e) {
 
 // Activate event
 self.addEventListener("activate", function(e) {
-    console.log("Alloy service worker activation");
+    console.log("Sherwin service worker activation");
     e.waitUntil(
         caches.keys().then(function(keyList) {
             return Promise.all(
                 keyList.map(function(key) {
                     if (key !== cacheName) {
-                        console.log("Alloy old cache removed", key);
+                        console.log("Sherwin old cache removed", key);
                         return caches.delete(key);
                     }
                 })
